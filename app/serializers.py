98ccb8d9cb0e526d40b71780e938f82dd7df3127
xiidot1303 <xii1303@inbox.ru>
2024-12-03 17:13:20 +0500

    product list api

---
+++ app/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from adrf.serializers import Serializer, ModelSerializer
+from app.models import *
+from bot.models import Bot_user
+
+
+class ProductSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
